fcdb84ddc6de4ac966034ec8e03f3308fcf8ef7b
Victor Delgado <vdel26@gmail.com>
2014-02-11 11:27:14 +0100

    fjs 14 – tail call optimization with trampolines

--- fjs-14.js
+++ fjs-14.js
@@ -1,15 +1,30 @@
-function repeat(operation, num) {
-  // Modify this so it doesn't cause a stack overflow!
-  if (num <= 0) return;
-  operation();
-  return repeat(operation, --num);
-}
+// JS doesn't implement tail call optimizations
+// however a trampoline can be used to avoid exhausting the stack
+// ex: http://taylodl.wordpress.com/2013/06/07/functional-javascript-tail-call-optimization-and-trampolines/
 
+// not optimized
+// function repeat(operation, num) {
+//   if (num <= 0) return;
+//   operation();
+//   return repeat(operation, --num);
+// }
+
+// optimized
+function repeat (operation, num) {
+  if (num <= 0) return num;
+  operation();
+  return repeat.bind(undefined, operation, --num);
+}
+  
+
+// repeatedly call the function until it becomes a value
 function trampoline(fn) {
-  // You probably want to implement a trampoline!
+  while (fn && fn instanceof Function) {
+    fn = fn.apply(undefined, arguments);
+  }
+  return fn;
 }
 
 module.exports = function(operation, num) {
-  // You probably want to call your trampoline here!
-  return repeat(operation, num);
+  return trampoline(repeat.bind(undefined, operation, num));
 };
